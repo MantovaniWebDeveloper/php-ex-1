119690927c6b485ca6761c8dd683c419e7a2d16b
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-21 14:25:42 +0100

    Stampata la lunghezza della stringa

--- esercizioDue.php
+++ esercizioDue.php
@@ -1,7 +1,23 @@
 <?php
+  /*
+  Esercizio due
+  Censuratore in php
+  */
 
   $paragrafo = "Come parte di una ristrutturazione delle finanze della Formula 1 dall'inizio del 2013, la FIA ha calcolato la quota di iscrizione per le squadre in base al numero di punti ottenuti nella stagione precedente.";
 
+  $lunghezzaStringa = strlen($paragrafo);
+
+  $badword = "FIA";
+
   echo $paragrafo;
+
+  echo "<br />";
+
+  echo "<br />";
+
+  echo "La lunghezza della stringa è: $lunghezzaStringa";
+
+
 
  ?>
