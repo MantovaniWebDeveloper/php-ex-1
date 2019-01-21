7c71df8616e429668f5dbf6cd93f6c760c88ed56
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-21 14:19:07 +0100

    Stampato il paragafro a schermo con echo

---
+++ esercizioDue.php
@@ -0,0 +1,7 @@
+<?php
+
+  $paragrafo = "Come parte di una ristrutturazione delle finanze della Formula 1 dall'inizio del 2013, la FIA ha calcolato la quota di iscrizione per le squadre in base al numero di punti ottenuti nella stagione precedente.";
+
+  echo $paragrafo;
+
+ ?>
